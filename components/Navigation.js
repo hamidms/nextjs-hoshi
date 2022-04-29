052036fe4c6941e2507cc0af585630b31701bce8
hamidmsukardi <hamidmsukardi@gmail.com>
2022-04-29 15:45:55 +0700

    removing map

--- components/Navigation.js
+++ components/Navigation.js
@@ -4,14 +4,8 @@ import Link from "next/link";
 export default function Navigation() {
   return (
     <nav className="flex justify-end">
-    {[
-      ['Home', '/'],
-      ['Posts', '/posts'],
-      ['Projects', '/projects'],
-      ['About', '/about'],
-    ].map(([title, url]) => (
-      <Link href={url} ><a className="rounded-lg px-3 py-2 text-slate-700 hover:bg-slate-200 hover:text-slate-900">{title}</a></Link>
-    ))}
+      <Link href='/' ><a className="rounded-lg px-3 py-2 text-slate-700 hover:bg-slate-200 hover:text-slate-900">Home</a></Link>
+      <Link href='/post' ><a className="rounded-lg px-3 py-2 text-slate-700 hover:bg-slate-200 hover:text-slate-900">Post</a></Link>
     </nav>
   )
 }
